3c927ba0cfc7e4c27157c1fdab17f5384c1f6c62
shitalkumar58627 <53770105+shitalkumar58627@users.noreply.github.com>
2023-01-17 15:42:47 +0530

    Update variable.tf
    dddd

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 variable "subnet_id_1" {
   type    = string
-  default = "subnet-0f267b82fd15ae917"
+  default = "subnet-0e815b5ec7862538b"
 }
 
 variable "subnet_id_2" {
   type    = string
-  default = "subnet-0f5b7fdfc4873cdcf"
+  default = "subnet-0651489a4475515ad"
 }
